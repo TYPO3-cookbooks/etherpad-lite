93fa24cd70b77e147942d3038d3e2bb78b084bbd
Steffen Gebert <steffen.gebert@typo3.org>
2017-02-14 14:27:06 +0100

    [TASK] Switch to v2 pipeline

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "steffen.gebert@typo3.org"
 license          "Apache 2.0"
 description      "Installs/Configures etherpad-lite"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.0.0"
+version          IO.read(File.join(File.dirname(__FILE__), 'VERSION')) rescue '0.0.1'
 
 depends "build-essential", "< 5.0.0"
 depends "database", "~> 1.3.12"
